fc923f09560ac25bb9ebcfdaffe935d288d2033b
JOSETABOSA47 <josetabosa47@gmail.com>
2024-09-14 13:04:54 -0300

    first commit 03

--- vendas.py
+++ vendas.py
@@ -6,75 +6,73 @@ import http.client
 import json
 import locale
 
-from streamlit_card import card
+# from streamlit_card import card
 
-# Configurar a localidade para o Brasil
-locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+# # Configurar a localidade para o Brasil
+# locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
-# st.title(':orange[VENDAS DE HOJE NO BLING]')
-st.markdown(
-    """
-    <style>
-    .title {
-        text-align: center;
-        color: #7f4307;
-    }
-    </style>
-    <h1 class="title">VENDAS GERAL</h1>
-    """,
-    unsafe_allow_html=True
-)
+# # st.title(':orange[VENDAS DE HOJE NO BLING]')
+# st.markdown(
+#     """
+#     <style>
+#     .title {
+#         text-align: center;
+#         color: #7f4307;
+#     }
+#     </style>
+#     <h1 class="title">VENDAS GERAL</h1>
+#     """,
+#     unsafe_allow_html=True
+# )
 
-conn_mamoeiroce = http.client.HTTPSConnection("api.bling.com.br")
-payload = ''
-headers = {
-  'Accept': 'application/json',
-  'Authorization': 'Bearer 31fbc75b555ef5b18d19c1938ef6d8a1476b0c9d',
-  'Cookie': 'PHPSESSID=qv71pbm1mutnjkint5g7dtte6p'
-}
-conn_mamoeiroce.request("GET", "/Api/v3/pedidos/vendas?pagina=1&limite=100", payload, headers)
-res = conn_mamoeiroce.getresponse()
-databling_mamoeiroce = json.loads(res.read())
-conn_mamoeiroce.close()
-totalvendas_mamoeiroce = 0
-for tem_mamoeiroce in databling_mamoeiroce["data"]:
-    totalvendas_mamoeiroce = totalvendas_mamoeiroce + tem_mamoeiroce["total"]
+# conn_mamoeiroce = http.client.HTTPSConnection("api.bling.com.br")
+# payload = ''
+# headers = {
+#   'Accept': 'application/json',
+#   'Authorization': 'Bearer 31fbc75b555ef5b18d19c1938ef6d8a1476b0c9d',
+#   'Cookie': 'PHPSESSID=qv71pbm1mutnjkint5g7dtte6p'
+# }
+# conn_mamoeiroce.request("GET", "/Api/v3/pedidos/vendas?pagina=1&limite=100", payload, headers)
+# res = conn_mamoeiroce.getresponse()
+# databling_mamoeiroce = json.loads(res.read())
+# conn_mamoeiroce.close()
+# totalvendas_mamoeiroce = 0
+# for tem_mamoeiroce in databling_mamoeiroce["data"]:
+#     totalvendas_mamoeiroce = totalvendas_mamoeiroce + tem_mamoeiroce["total"]
 
-# Dados para gerar cartões
-data_list = [
-    {"title": "MAMOEIRO SP", "value": 12345.67},
-    {"title": "MAMOEIRO CE", "value": totalvendas_mamoeiroce},
-    {"title": "WEB GLAMOUR CE", "value": 23456.78},
-    {"title": "LUCAS FREE SHOP SP", "value": 23456.78},
-]
+# # Dados para gerar cartões
+# data_list = [
+#     {"title": "MAMOEIRO SP", "value": 12345.67},
+#     {"title": "MAMOEIRO CE", "value": totalvendas_mamoeiroce},
+#     {"title": "WEB GLAMOUR CE", "value": 23456.78},
+#     {"title": "LUCAS FREE SHOP SP", "value": 23456.78},
+# ]
 
-# Criar colunas dinamicamente
-cols = st.columns(4)  # Supondo que você quer 3 colunas
+# # Criar colunas dinamicamente
+# cols = st.columns(4)  # Supondo que você quer 3 colunas
 
-for i, data in enumerate(data_list):
-    formatted_value = locale.currency(data["value"], symbol=False, grouping=True)
-    with cols[i % 4]:  # Distribuir os cartões entre as colunas
-        card(
-            key=f"card_{i}",
-            title=data["title"],
-            text=formatted_value,
-            styles={
-                "card": {
-                    "width": "120px",
-                    "height": "80px",
-                    "background-color": "#FFA500",  # Cor laranja
-                    "color": "white",  # Cor do texto
-                    "border-radius": "20px",  # Bordas arredondadas
-                    "box-shadow": "0 0 10px rgba(0,0,0,0.5)"  # Sombra
-                },
-                "title": {
-                    "font-size": "8px",  # Ajusta o tamanho do texto do título
-                    "font-weight": "bold",  # Negrito (opcional)
-                }
-            }
-        )
-
-
+# for i, data in enumerate(data_list):
+#     formatted_value = locale.currency(data["value"], symbol=False, grouping=True)
+#     with cols[i % 4]:  # Distribuir os cartões entre as colunas
+#         card(
+#             key=f"card_{i}",
+#             title=data["title"],
+#             text=formatted_value,
+#             styles={
+#                 "card": {
+#                     "width": "120px",
+#                     "height": "80px",
+#                     "background-color": "#FFA500",  # Cor laranja
+#                     "color": "white",  # Cor do texto
+#                     "border-radius": "20px",  # Bordas arredondadas
+#                     "box-shadow": "0 0 10px rgba(0,0,0,0.5)"  # Sombra
+#                 },
+#                 "title": {
+#                     "font-size": "8px",  # Ajusta o tamanho do texto do título
+#                     "font-weight": "bold",  # Negrito (opcional)
+#                 }
+#             }
+#         )
 
 
 
@@ -83,33 +81,35 @@ for i, data in enumerate(data_list):
 
 
 
-# DATE_COLUMN = 'date/time'
-# DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-#             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-# @st.cache_data
-# def load_data(nrows):
-#     data = pd.read_csv(DATA_URL, nrows=nrows)
-#     lowercase = lambda x: str(x).lower()
-#     data.rename(lowercase, axis='columns', inplace=True)
-#     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-#     return data
 
-# data_load_state = st.text('Loading data...')
-# data = load_data(10000)
-# data_load_state.text("Done! (using st.cache_data)")
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-# if st.checkbox('Show raw data'):
-#     st.subheader('Raw data')
-#     st.write(data)
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
 
-# st.subheader('Number of pickups by hour')
-# hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-# st.bar_chart(hist_values)
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache_data)")
 
-# # Some number in the range 0-23
-# hour_to_filter = st.slider('hour', 0, 23, 17)
-# filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
 
-# st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-# st.map(filtered_data)
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
