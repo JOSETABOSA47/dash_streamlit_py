fef367c0076c2be3654e940228ad78d112a63eb6
jt_sobrinho <josetabosa47@gmail.com>
2024-09-14 20:50:48 +0000

    mostrando os pedidos da Mamoeiro CE da data atual

--- vendas.py
+++ vendas.py
@@ -3,37 +3,102 @@ import pandas as pd
 import numpy as np
 import http.client
 import json
+import time
 
+from operator import sub
+from datetime import datetime, date, timedelta
 from streamlit_card import card
 
-# st.title(':orange[VENDAS DE HOJE NO BLING]')
-st.markdown(
-    """
-    <style>
-    .title {
-        text-align: center;
-        color: #7f4307;
-    }
-    </style>
-    <h1 class="title">VENDAS DE HOJE NO BLING</h1>
-    """,
-    unsafe_allow_html=True
-)
+# Obtém a data e hora atual
+data_hora_atual = datetime.now()
+# Subtrair 3 horas usando timedelta
+data_hora_modificada = data_hora_atual - timedelta(hours=3)
+# Formatar a data e hora no formato desejado
+data_formatada = data_hora_modificada.strftime("%d/%m/%Y %H:%M:%S")
+dataatual = date.today()
+dataantiga = sub(date.today(), timedelta(60))
+# dataatual_api = f"{dataatual.day}/{dataatual.month}/{dataatual.year}"
+dataatual_api = f"{dataatual.year}-{dataatual.month}-{dataatual.day}"
+dataantiga_api = f"{dataantiga.day}/{dataantiga.month}/{dataantiga.year}"
 
-conn_mamoeiroce = http.client.HTTPSConnection("api.bling.com.br")
-payload = ''
-headers = {
-  'Accept': 'application/json',
-  'Authorization': 'Bearer 31fbc75b555ef5b18d19c1938ef6d8a1476b0c9d',
-  'Cookie': 'PHPSESSID=qv71pbm1mutnjkint5g7dtte6p'
-}
-conn_mamoeiroce.request("GET", "/Api/v3/pedidos/vendas?pagina=1&limite=100", payload, headers)
-res = conn_mamoeiroce.getresponse()
-databling_mamoeiroce = json.loads(res.read())
-conn_mamoeiroce.close()
+# st.title(':orange[VENDAS DE HOJE NO BLING]')
+# st.markdown(
+#     """
+#     <style>
+#     .title {
+#         text-align: center;
+#         color: #7f4307;
+#     }
+#     </style>
+#     <h1 class="title">VENDAS DE HOJE NO BLING</h1>
+#     """,
+#     unsafe_allow_html=True
+# )
+
+lista_pedidos_mamoeiroce = []
+lista_produtos_mamoeiroce = []
 totalvendas_mamoeiroce = 0
-for tem_mamoeiroce in databling_mamoeiroce["data"]:
-    totalvendas_mamoeiroce = totalvendas_mamoeiroce + tem_mamoeiroce["total"]
+
+@st.cache_data
+def load_data(nrows):
+#     # data = pd.read_csv(DATA_URL, nrows=nrows)
+#     # lowercase = lambda x: str(x).lower()
+#     # data.rename(lowercase, axis='columns', inplace=True)
+#     # data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+
+    apimemoeiroce = f"/Api/v3/pedidos/vendas?pagina=1&limite=200&dataInicial={dataatual_api}"
+    conn_mamoeiroce = http.client.HTTPSConnection("api.bling.com.br")
+    payload = ''
+    headers = {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer 444c1e677ebe0e68b745144e48545e9017a5c4bb',
+        'Cookie': 'PHPSESSID=5rduhjhej10cvkb8a6jgljkorv'
+    }
+    conn_mamoeiroce.request("GET", apimemoeiroce, payload, headers)
+    res = conn_mamoeiroce.getresponse()
+    databling_mamoeiroce = json.loads(res.read())
+    conn_mamoeiroce.close()
+    for tem_mamoeiroce in databling_mamoeiroce["data"]:
+        # totalvendas_mamoeiroce = totalvendas_mamoeiroce + tem_mamoeiroce["total"]
+        lista_pedidos_mamoeiroce.append(tem_mamoeiroce["id"])
+
+    CONTA = 0
+    for pedidos in lista_pedidos_mamoeiroce:
+        CONTA = CONTA + 1
+        print(pedidos)
+        apipedidos= f"/Api/v3/pedidos/vendas/{pedidos}"
+        conn_pedidos = http.client.HTTPSConnection("api.bling.com.br")
+        payload = ''
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer 444c1e677ebe0e68b745144e48545e9017a5c4bb',
+            'Cookie': 'PHPSESSID=5rduhjhej10cvkb8a6jgljkorv'
+        }
+        conn_pedidos.request("GET", apipedidos, payload, headers)
+        res = conn_pedidos.getresponse()
+        print(f"{CONTA} - STATUS - ", res.status, "E CONECÇÃO - ", res.reason)
+        data_mamoeiroce = json.loads(res.read())
+        conn_pedidos.close()
+        time.sleep(0.5)
+
+        try:
+            for produtos in data_mamoeiroce["data"]["itens"]:
+                print(produtos)
+                lista_produtos_mamoeiroce.append(produtos)
+        except:
+            print("ERRRRRRROOOOOO ", pedidos)
+            continue
+        # print(data_mamoeiroce)
+        # print(apipedidos)
+        # break
+    return lista_produtos_mamoeiroce
+
+data_load_state = st.text('Carregando dados...')
+data = load_data(10000)
+data_load_state.text(f"ATUALIZAÇAO {data_formatada}")
+
+for tem in lista_produtos_mamoeiroce:
+    totalvendas_mamoeiroce = totalvendas_mamoeiroce + tem["valor"]
 
 # Dados para gerar cartões
 data_list = [
@@ -70,34 +135,27 @@ for i, data in enumerate(data_list):
         )
 
 
+# Loop para exibir cada produto
+for produto in lista_produtos_mamoeiroce:
+    # Criar uma linha para cada produto
+    col1, col2, col3 = st.columns([1, 3, 2])  # Ajuste a proporção das colunas conforme necessário
+    
+    with col1:
+        # Exibir a imagem do produto
+        st.image(f"https://midia.mamoeiro.com.br/{produto['codigo']}-1.jpg", width=100)
+        
+    with col2:
+        # Exibir o nome e quantidade de vendas
+        st.subheader(produto["descricao"])
+        st.subheader(produto["codigo"])
+        
+    with col3:
+        # Exibir o preço formatado
+        st.write(" ")
+        st.write(f"Quantidade: {produto['quantidade']}")
+        st.write(f"Preço: R$ {produto['valor']:.2f}")
+        st.write(f"MAMOEIRO CE")
+    
+    # Adicionar uma linha divisória entre os produtos (opcional)
+    st.markdown("---")
 
-# DATE_COLUMN = 'date/time'
-# DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-#             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
-# @st.cache_data
-# def load_data(nrows):
-#     data = pd.read_csv(DATA_URL, nrows=nrows)
-#     lowercase = lambda x: str(x).lower()
-#     data.rename(lowercase, axis='columns', inplace=True)
-#     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-#     return data
-
-# data_load_state = st.text('Loading data...')
-# data = load_data(10000)
-# data_load_state.text("Done! (using st.cache_data)")
-
-# if st.checkbox('Show raw data'):
-#     st.subheader('Raw data')
-#     st.write(databling_mamoeiroce)
-
-# st.subheader('Number of pickups by hour')
-# hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-# st.bar_chart(hist_values)
-
-# # Some number in the range 0-23
-# hour_to_filter = st.slider('hour', 0, 23, 17)
-# filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-# st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-# st.map(filtered_data)
